abf9b3de6ebeb22ab06e29913f1fe605b97ed364
Colin Stodd <cbstodd@gmail.com>
2016-01-10 10:49:46 -0500

    add admin email notification when new user signs up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,6 @@ class UserMailer < ApplicationMailer
   #   en.user_mailer.account_activation.subject
   #
 
-
   def account_activation(user)
     @user = user
     mail(to: user.email, subject: "Welcome to Polis",
