768bf87e3c37cf846314717a578bea2e24ec6ad4
Colin Stodd <cbstodd@gmail.com>
2016-01-10 12:50:56 -0500

    mailer bug

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,14 @@
 class UserMailer < ApplicationMailer
+  #
+  # def account_activation(user)
+  #   @user = user
+  #   mail(to: user.email, subject: "Welcome to Polis",
+  #        bcc: ["cbstodd@me.com", "New Polis member"])
+  # end
 
   def account_activation(user)
     @user = user
-    mail(to: user.email, subject: "Welcome to Polis",
-         bcc: ["cbstodd@me.com", "New Polis member"])
+    mail to: user.email, subject: "Welcome to Polis"
   end
 
   def password_reset(user)
