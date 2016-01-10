1ff4299ec9c3add492e1d6a32c1686863425f6cd
Colin Stodd <cbstodd@gmail.com>
2016-01-10 12:44:16 -0500

    mailer bug

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,8 @@ class UserMailer < ApplicationMailer
 
   def account_activation(user)
     @user = user
-    mail(to: user.email, subject: "Welcome #{user.username}",
-         bcc: ["cbstodd@gmail.com", "New Polis member"])
+    mail(to: user.email, subject: "Welcome to Polis",
+         bcc: ["cbstodd@me.com", "New Polis member"])
   end
 
   def password_reset(user)
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -6,7 +6,7 @@ class UserMailerTest < ActionMailer::TestCase
     user = users(:tommy)
     user.activation_token = User.new_token
     mail = UserMailer.account_activation(user)
-    assert_equal "Welcome #{user.username}",      mail.subject
+    assert_equal "Welcome to Polis",      mail.subject
     assert_equal [user.email],            mail.to
     assert_equal ["noreply@polis.com"],   mail.from
     assert_match user.username,           mail.body.encoded
