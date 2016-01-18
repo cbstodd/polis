f70a6f184f968260014f35cdbdd2807fd1de994b
Colin Stodd <cbstodd@gmail.com>
2016-01-17 22:16:01 -0500

    css glyphicons not working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets
 //= require jquery.ui.datepicker
 //= require ckeditor/init
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
