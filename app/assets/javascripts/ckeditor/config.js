ceb0f7453c0c9687434f07c02bc9bd8f7b8565b7
Colin Stodd <cbstodd@gmail.com>
2016-01-26 19:24:11 -0500

    fixed ckeditor issue where it would dissapear on postbacks

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,22 +1,29 @@
-CKEDITOR.editorConfig = function (config) {
-    //config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
-    //config.filebrowserFlashBrowseUrl = "/ckeditor/attachment_files";
-    //config.filebrowserFlashUploadUrl = "/ckeditor/attachment_files";
-    //config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
-    //config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
-    //config.filebrowserImageUploadUrl = "/ckeditor/pictures";
-    //config.filebrowserUploadUrl = "/ckeditor/attachment_files";
+// page:load tells turbolinks that page was reloaded
+//  to trigger jquery.
+//$(document).on('ready page:update', function() {
+//    console.log('page and turbolinks was updated');
 
-    config.toolbar = [
-        //{ name: 'insert',  items: ["Image"] },
-        { name: 'basicstyles',  items: ["Bold",  "Italic",  "Underline",  "Strike",  "-",  "Subscript",  "Superscript"] },
-        { name: 'links',  items: ["Link",  "Unlink"] },
-        { name: 'paragraph',  items: ["NumberedList",  "BulletedList",  "Outdent", "Indent", "Blockquote"] }
-        //{ name: 'styles', items: [ 'Font', 'FontSize' ] },
-        //{ name: 'font', items: [ 'Font', 'FontSize' ] },
-        //{ name: 'colors', items: [ 'TextColor','BGColor' ] },
-    ];
-    //config.toolbar = "simple";
+    CKEDITOR.editorConfig = function (config) {
+        //config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
+        //config.filebrowserFlashBrowseUrl = "/ckeditor/attachment_files";
+        //config.filebrowserFlashUploadUrl = "/ckeditor/attachment_files";
+        //config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
+        //config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
+        //config.filebrowserImageUploadUrl = "/ckeditor/pictures";
+        //config.filebrowserUploadUrl = "/ckeditor/attachment_files";
+
+        config.toolbar = [
+            //{ name: 'insert',  items: ["Image"] },
+            { name: 'basicstyles',  items: ["Bold",  "Italic",  "Underline",  "Strike",  "-",  "Subscript",  "Superscript"] },
+            { name: 'links',  items: ["Link",  "Unlink"] },
+            { name: 'paragraph',  items: ["NumberedList",  "BulletedList",  "Outdent", "Indent", "Blockquote"] },
+            //{ name: 'styles', items: [ 'Font', 'FontSize' ] },
+            //{ name: 'font', items: [ 'Font', 'FontSize' ] },
+            { name: 'colors', items: [ 'TextColor','BGColor' ] }
+        ];
+        //config.toolbar = "simple";
 
 
-};
+    };
+
+//});//End jQuery
